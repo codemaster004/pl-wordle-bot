703823fb4a56d7e9a9d1190238943d465bb23f9a
Filip Dabkowski <filip.dabkowski@gmail.com>
2023-03-26 10:41:43 +0200

    almost works

--- src/memory_table.py
+++ src/memory_table.py
@@ -4,13 +4,15 @@ import itertools
 import math
 import sqlite3
 from time import perf_counter
+import redis
+import wolframclient
 
 from main import compare_word_with_key
 
 conn = sqlite3.connect('patterns.db')
 c = conn.cursor()
 
-redis_distribution = redis.Redis(host='localhost', port=6379, db=5)
+redis_patterns = redis.Redis(host='localhost', port=6379, db=1)
 
 
 with open("data/words.json") as file:
@@ -25,38 +27,37 @@ def initial_table_creation():
 	# conn.commit()
 	
 
-def add_word_patterns(pattern_data):
-	try:
-		c.executemany('INSERT INTO pattern_table VALUES (?, ?, ?, ?)', pattern_data)
-		conn.commit()
-	except sqlite3.IntegrityError:
-		print('Those values are already added')
+def add_word_patterns(data_dict):
+	redis_patterns.hset(data_dict['key'], mapping=data_dict['patterns'])
 
 
 # initial_table_creation()
 
-def calc_all_patterns():
-	for checking_word in words:
-		temp_table = {
-			'words_combination': [],
-			'checking_word': [],
-			'target_word': [],
-			'pattern': []
+def calc_all_patterns(words):
+	calculate_words = redis_patterns.keys('*')
+	calculate_words = [word.decode('utf-8').lower() for word in calculate_words]
+	missing_words = list(set(words) - set(calculate_words))
+	missing_words.sort(reverse=True)
+	print(len(missing_words), "are missing!")
+	for checking_word in missing_words:
+		temp_dict = {
+			'key': checking_word,
+			'patterns': {str(pattern): [] for pattern in list(itertools.product(range(3), repeat=len(checking_word)))}
 		}
 		for key_word in words:
 			generated_pattern = compare_word_with_key(key_word, checking_word)
-			temp_table['words_combination'].append(f'{checking_word}-{key_word}')
-			temp_table['checking_word'].append(checking_word)
-			temp_table['target_word'].append(key_word)
-			temp_table['pattern'].append(str(generated_pattern))
+			temp_dict['patterns'][str(generated_pattern)].append(key_word)
 		
-		transformed_table = list(zip(temp_table['checking_word'], temp_table['target_word'], temp_table['pattern']))
-		add_word_patterns(transformed_table)
+		for pattern in temp_dict['patterns']:
+			temp_dict['patterns'][pattern] = json.dumps(temp_dict['patterns'][pattern])
+		
+		add_word_patterns(temp_dict)
 
 
-def calc_entropy(words):
-	for word in words:
-		sql = ''''''
+# start = perf_counter()
+calc_all_patterns(words)
+# end = perf_counter()
+# print(f"Patterns generated in {(end - start) * 1000:0.0f} ms")
 
 
 # df = pd.DataFrame(memory_table)
