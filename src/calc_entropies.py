c104e613e652c4655a9e90c5c5c1b2d349485839
Filip Dabkowski <filip.dabkowski@gmail.com>
2023-03-26 17:11:01 +0200

    first version that actually words

--- src/calc_entropies.py
+++ src/calc_entropies.py
@@ -5,6 +5,7 @@ import json
 import threading
 import numpy as np
 from datetime import datetime
+import psycopg2
 
 from main import create_pattern_chart
 
@@ -14,16 +15,30 @@ redis_patterns = redis.Redis(host='localhost', port=6379, db=1)
 # redis_entropies = redis.Redis(host='localhost', port=6379, db=4)
 # redis_distribution = redis.Redis(host='localhost', port=6379, db=5)
 
+conn = psycopg2.connect(
+	host="localhost",
+	port=5432,
+	database="wordle",
+	user="wordle",
+	password="wordle"
+)
+cur = conn.cursor()
+
+with open("data/words.json") as file:
+	data = json.load(file)
+	full_word_list = data['slowa']
+
 
 def calc_entropy_for_words(word_list):
-	for word in word_list[:1]:
-		start = time.perf_counter()
+	global full_word_list
+	for word in word_list[:]:
+		# start = time.perf_counter()
 		pattern_distribution = redis_patterns.hgetall(word)
 		pattern_distribution = {k.decode('utf-8'): json.loads(v) for k, v in pattern_distribution.items()}
 		# print(pattern_distribution)
-		score = create_pattern_chart(word, word_list, pattern_distribution)
-		end = time.perf_counter()
-		print(f"Done in {(end - start) * 1000:0.0f} ms")
+		score = create_pattern_chart(word, full_word_list, pattern_distribution)
+		# end = time.perf_counter()
+		# print(f"Done in {(end - start) * 1000:0.0f} ms")
 		print(word, score)
 		redis_sorted.zadd('firstEntropy', {word: score})
 		# patterns = dist['patterns']
@@ -33,6 +48,28 @@ def calc_entropy_for_words(word_list):
 		# redis_distribution.hset(dist['key'].upper(), mapping=patterns)
 		# redis_entropies.set(dist['key'].upper(), dist['entropy'])
 		# redis_sorted.zadd('entropy', {dist['key'].upper(): dist['entropy']})
+
+
+def calc_entropy_for_words_sql(word_list):
+	global full_word_list
+	for word in word_list[:]:
+		cur.execute(f"""select checking_word_pattern, key_words from pattern_table where checking_word_pattern like '{word}-%'""")
+		temp_distribution = cur.fetchall()
+		pattern_distribution = {}
+		# print(temp_distribution)
+		for result in temp_distribution:
+			key = result[0].split('-')[1]
+			value = json.loads(json.loads(result[1]))
+			# print(value)
+			pattern_distribution[key] = value
+		
+		# pattern_distribution = {k.decode('utf-8'): json.loads(v) for k, v in pattern_distribution.items()}
+		# print(pattern_distribution)
+		score = create_pattern_chart(word, full_word_list, pattern_distribution)
+		# end = time.perf_counter()
+		# print(f"Done in {(end - start) * 1000:0.0f} ms")
+		print(word, score)
+		redis_sorted.zadd('firstEntropy', {word: score})
 
 
 def calc_rest_of_entropies():
@@ -46,8 +83,12 @@ def calc_rest_of_entropies():
 	# decode utf-8 because .keys() return value as bits
 	calculate_words = [word.decode('utf-8').lower() for word in calculate_words]
 	
+	cur.execute("""select word from saved_words;""")
+	sql_words = cur.fetchall()
+	sql_words = [word[0] for word in sql_words]
 	# Run the calculation for them
 	# Now i have to pick one with the highest entropy. so calculate the entropy.
+	# calc_entropy_for_words_sql(sql_words)
 	calc_entropy_for_words(calculate_words)
 
 
--- src/game_simulation.py
+++ src/game_simulation.py
@@ -1,11 +1,22 @@
 import json
 import random
 import redis
+import psycopg2
 
-from main import compare_word_with_key, does_word_fit
+from main import compare_word_with_key, does_word_fit, create_pattern_chart
 
 
-redis_sorted = redis.Redis(host='localhost', port=6379, db=3)
+redis_sorted = redis.Redis(host='localhost', port=6379, db=0)
+redis_patterns = redis.Redis(host='localhost', port=6379, db=1)
+
+conn = psycopg2.connect(
+	host="localhost",
+	port=5432,
+	database="wordle",
+	user="wordle",
+	password="wordle"
+)
+cur = conn.cursor()
 
 
 class Game:
@@ -57,18 +68,96 @@ def simulate_bot(words, pattern, check_word):
 	return words_passed
 
 
+class Bot:
+	
+	def __init__(self, words):
+		self.words_available = words
+		self.previous_pattern = []
+		self.sql_words = []
+		self.redis_words = []
+	
+	def load_word_lists(self):
+		with open("data/extended_words.json") as file:
+			data = json.load(file)
+			self.sql_words = data['slowa']
+		
+		with open("data/short_words.json") as file:
+			data = json.load(file)
+			self.redis_words = data['slowa']
+	
+	def recalculate_entropies(self):
+		new_entropies = {}
+		for word in self.words_available:
+			pattern_distribution = redis_patterns.hgetall(word)
+			pattern_distribution = {k.decode('utf-8'): json.loads(v) for k, v in pattern_distribution.items()}
+			for pattern, word_list in pattern_distribution.items():
+				pattern_distribution[pattern] = [w for w in word_list if w in self.words_available]
+			score = create_pattern_chart(word, self.words_available, pattern_distribution)
+			new_entropies[word] = score
+		
+		new_entropies = sorted(new_entropies.items(), key=lambda x: x[1], reverse=True)
+		return new_entropies
+	
+	def choose_word(self):
+		if not wordle.can_play():
+			return
+		if not self.previous_pattern:
+			top_scores = redis_sorted.zrevrange('firstEntropy', 0, 9)
+			top_pick = top_scores[0].decode('utf-8')
+			print(top_pick)
+			guessed_pattern = wordle.turn(top_pick)
+			print(guessed_pattern)
+			self.previous_pattern.append(guessed_pattern)
+			
+			new_words = []
+			for word in self.words_available:
+				if does_word_fit(word, guessed_pattern, top_pick):
+					new_words.append(word)
+			print(len(new_words), "remaining", "\n")
+			self.words_available = new_words
+			return
+		else:
+			# print(self.words_available)
+			entropies = self.recalculate_entropies()
+			top_pick = entropies[0][0]
+			print(top_pick)
+			guessed_pattern = wordle.turn(top_pick)
+			print(guessed_pattern)
+			self.previous_pattern.append(guessed_pattern)
+			
+			new_words = []
+			for word in self.words_available:
+				if does_word_fit(word, guessed_pattern, top_pick):
+					new_words.append(word)
+			print(len(new_words), "remaining", "\n")
+			self.words_available = new_words
+			
+			return
+			
+
 if __name__ == '__main__':
 	wordle = Game()
-	wordle.load_words('simple_word.json')
+	wordle.load_words('words.json')
 	wordle.start()
+	bot = Bot(wordle.words)
 	
-	new_words = wordle.words
-	while wordle.can_play():
-		top_scores = redis_sorted.zrevrange('entropy', 0, 4)
-		top_scores = [word.decode('utf-8').lower() for word in top_scores]
-		print(top_scores)
-		pattern = wordle.turn(top_scores[0])
-		print(pattern)
-		new_words = simulate_bot(new_words, pattern, top_scores[0])
-		print(len(new_words))
-		wordle.lost = True
+	# new_words = wordle.words
+	bot.choose_word()
+	bot.choose_word()
+	bot.choose_word()
+	bot.choose_word()
+	bot.choose_word()
+	bot.choose_word()
+	# while wordle.can_play():
+	# 	if len(wordle.ties) == 0:
+	# 		bot.choose_word()
+	# 		
+	# 	else:
+	# 		# top_scores = redis_sorted.zrevrange('entropy', 0, 4)
+	# 		# top_scores = [word.decode('utf-8').lower() for word in top_scores]
+	# 		# print(top_scores)
+	# 		# pattern = wordle.turn(top_scores[0])
+	# 		# print(pattern)
+	# 		# new_words = simulate_bot(new_words, pattern, top_scores[0])
+	# 		# print(len(new_words))
+	# 		wordle.lost = True
