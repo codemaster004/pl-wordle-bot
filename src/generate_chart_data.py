1396670486ce826259fe6b5835ce9782be3059e9
Filip Dabkowski <filip.dabkowski@gmail.com>
2023-03-30 08:16:06 +0200

    add word, create percentages with visualization

--- src/generate_chart_data.py
+++ src/generate_chart_data.py
@@ -1,10 +1,20 @@
 import redis
 import json
+import psycopg2
 
 
 redis_patterns = redis.Redis(host='localhost', port=6379, db=1)
 
-VISUAL_WORD = 'zarys'
+conn = psycopg2.connect(
+	host="localhost",
+	port=5432,
+	database="wordle",
+	user="wordle",
+	password="wordle"
+)
+cur = conn.cursor()
+
+VISUAL_WORD = 'towar'.lower()
 
 with open('data/words.json') as file:
 	words = json.load(file)['slowa']
@@ -12,6 +22,14 @@ with open('data/words.json') as file:
 n = len(words)
 
 pattern_chart_data = []
+
+cur.execute(f"select checking_word_pattern, key_words from pattern_table where checking_word_pattern like '{VISUAL_WORD}-%';")
+result = cur.fetchall()
+for res in result:
+	pattern = res[0].split('-')[1]
+	int_pattern = [int(char) for char in pattern if char.isdigit()]
+	filtered_word = json.loads(json.loads(res[1]))
+	pattern_chart_data.append([int_pattern, len(filtered_word)])
 
 pattern_distribution = redis_patterns.hgetall(VISUAL_WORD)
 for pattern, pattern_words in pattern_distribution.items():
